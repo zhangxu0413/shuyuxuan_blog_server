aa5cd1fc45e145a1eadfaf7e227181e95bf63b06
zhangxu <zhangxu@91160.com>
2021-07-17 18:14:57 +0800

    题目接口

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '118.25.228.199',
+    port: 3306,
+    name: 'shuyuxuan_test',
+    username: 'zhangshuyu',
+    password: '654321',
+    database: 'shuyuxuan_test',
+    entities: [],
+    synchronize: true,
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
